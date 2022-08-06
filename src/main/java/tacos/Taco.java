2d40ce4f1c250c9591d5731ad9951ff5bea5b40c
evgeny4msk <suntech777@ya.ru>
2022-08-06 12:34:30 +0300

    еще немного кода 3

--- src/main/java/tacos/Taco.java
+++ src/main/java/tacos/Taco.java
@@ -4,9 +4,13 @@ package tacos;
 import java.util.List;
 
 import lombok.Data;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Data
 public class Taco {
+    @NonNull
     private String name;
+
     private List<Ingredient> ingredients;
 }
